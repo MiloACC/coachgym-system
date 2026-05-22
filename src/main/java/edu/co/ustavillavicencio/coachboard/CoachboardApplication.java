51e5fbf47dc5e88a7d6727a9f3f71cf364410fa1
Camilo Casallas <kamiloco.12346@gmail.com>
2026-05-22 10:08:56 -0500

    chore: initial commit with project structure

---
+++ src/main/java/edu/co/ustavillavicencio/coachboard/CoachboardApplication.java
@@ -0,0 +1,13 @@
+package edu.co.ustavillavicencio.coachboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/co/ustavillavicencio/coachboard/CoachboardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.ustavillavicencio.coachboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
